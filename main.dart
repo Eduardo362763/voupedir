80e9258eead44e0910ce9156fdf5dd8e1daf2596
Eduardo362763 <eduardo4532785@gmail.com>
2025-08-25 11:46:35 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import  'tela_login.dart';
+import 'usuario.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+
+    /*Usuario u = Usuario(
+      codigo: 1,
+      login: "Rodolfo123",
+      nome:"Rodolfo",
+      senha:"@senhaforte123"
+    );*/
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TelaLogin(),
+
+    );
+  }
+}
+
